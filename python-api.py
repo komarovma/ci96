3ded9916a5bacc45355bf033556d627110d2b2ba
Mikhail Komarov <komarovma@hotmail.com>
2022-02-04 12:34:39 +0300

    2

--- python-api.py
+++ python-api.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 api = Api(app)
 
 class Info(Resource):
-        def get(self):
-                    return {'version': 3, 'method': 'GET', 'message': 'Already started'}
+    def get(self):
+        return {'version': 3, 'method': 'GET', 'message': 'Already started'}
 
-                api.add_resource(Info, '/get_info')
+api.add_resource(Info, '/get_info')
 
-                if __name__ == '__main__':
-                         app.run(host='0.0.0.0', port='5290')
+if __name__ == '__main__':
+     app.run(host='0.0.0.0', port='5290')
